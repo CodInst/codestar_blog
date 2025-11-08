9131254c144f218b7e858204e42c1654f9017280
Mansour <Mansour@thecreativenerds.ie>
2025-11-08 19:54:34 +0000

    Models.py changes

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -17,11 +17,12 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["created_on"]
-
+        ordering = ["-created_on"]
+    
     def __str__(self):
         return f"{self.title} | written by {self.author}"
-    
+
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -36,5 +37,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
-
-    
+        
